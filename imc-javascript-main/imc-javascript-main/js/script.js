775e9d5e743ab26d1b48090786f136edc39a8521
GIULIA MARZANO MARTINIANO DOS SANTOS <54781547842@info.local>
2026-04-24 16:14:27 -0300

    24.04 - Finalização JAVASCRIPT

--- imc-javascript-main/imc-javascript-main/js/script.js
+++ imc-javascript-main/imc-javascript-main/js/script.js
@@ -1,79 +1,143 @@
 function calcular(){
-    // alert("Função Calcular rodando!!")
-    //pegar os valores dos campos
-
     const nome = document.getElementById("nome").value;
     const altura = parseFloat(document.getElementById("altura").value);
-    const peso = parseFloat(document.getElementById("peso").value);
+    const peso = parseFloat (document.getElementById("peso").value);
+   
 
-    // validar se esta tudo preenchido
-    if (nome.trim().length == 0 || isNaN(altura) || isNaN(peso)){
-        alert("Preencha todos os campos: Nome, Altura e Peso");
-        return false;
+
+    if(nome.trim().length == 0 || isNaN(altura) || isNaN(peso)){
+        alert("preencha todos os campos Nome, Altura, Peso");
+        return false
     }
 
-    const IMC = calcularIMC(altura,peso); //criando a constante imc
-    const textoSituacao = gerarTextoImc(IMC); //chama a função que gera o texto do IMC, 
-                         
-    // que chama o imc da constante de cima
-
-    console.log(nome);
-    console.log(altura);
-    console.log(peso);
-    console.log(IMC);
-    console.log(textoSituacao);
+    const IMC = calcularImc(altura, peso);
+    const textoSituacao = gerarTextoIMC(IMC);
 
 
-    //cadastrar a uma linha de tabelo no html
-    
-    const tabela = document.getElementById("cadastro");
+ 
+   
+const objIMC = {
+    nome : nome,
+    altura : altura,
+    peso : peso,
+    IMC : IMC,
+    situacao : textoSituacao
+};
 
-    tabela.innerHTML += //(innerHTML) acessa o html interno do id
-            `<tr>
-                <td>${nome}</td>
-                <td>${altura}</td>
-                <td>${peso}</td>
-                <td>${IMC}</td>
-                <td>${textoSituacao}</td> 
-            </tr>`;
+
+
+
+
+             
+const retorno = cadastrarNaAPI(objIMC);
+
+if(retorno){
+     const tabela = document.getElementById("cadastro");
+
+    tabela.innerHTML += `<td>${nome}</td>
+     <td>${altura}</td>
+     <td>${peso}</td>
+     <td>${IMC.toFixed(2)}</td>
+     <td>${textoSituacao}</td>`;
+
+    document.getElementById("nome").value = "";
+    document.getElementById("altura").value = "";
+    document.getElementById("peso").value = "";
+
+    alert(`${nome} foi cadastrado no banco:
+            Nome: ${nome}
+            IMC: ${IMC}
+            Situação: ${situacao}`
+        );
+
+
+}else{
+    alert("não foi possivel cadastrar")
 }
 
-    
 
+    function calcularImc(altura, peso){
+        return peso / (altura * altura);
+    }
 
+    function gerarTextoIMC(IMC){
+        if(IMC < 16){
+            return "Magreza grave"
+        }
+        else if(IMC < 17){
+            return "Magreza moderada"
+        }
+        else if(IMC < 18.5){
+            return "Magreza leve"
+        }
+        else if(IMC < 25){
+            return "Saudável"
+        }
+        else if(IMC < 30){
+            return "Sobrepeso"
+        }
+        else if(IMC < 35){
+            return "Obesidade Grau I"
+        }
+        else if(IMC < 40){
+            return "Obesidade Grau II"
+        }
+        else{
+            return "Obesidade Grau III"
+        }
 
-
-// calcular o IMC
-function calcularIMC(altura, peso){
-    return peso / (altura * altura);
+      
+    }
+  
 }
 
-function gerarTextoImc(IMC){
-    // Menor que 16 – Magreza grave;
-    // 16 a menor que 17 – Magreza moderada;
-    // 17 a menor que 18,5 – Magreza leve;
-    // 18,5 a menor que 25 – Saudável;
-    // 25 a menor que 30 – Sobrepeso;
-    // 30 a menor que 35 – Obesidade Grau I;
-    // 35 a menor que 40 – Obesidade Grau II (considerada severa);
-    // Maior que 40 – Obesidade Grau III (considerada mórbida).
 
-     if (IMC < 16) {
-        return "Magreza grave";
-    } else if (IMC < 17) {
-        return "Magreza moderada";
-    } else if (IMC < 18.5) {
-        return "Magreza leve";
-    } else if (IMC < 25) {
-        return "Saudável";
-    } else if (IMC < 30) {
-        return "Sobrepeso";
-    } else if (IMC < 35) {
-        return "Obesidade Grau I";
-    } else if (IMC < 40) {
-        return "Obesidade Grau II (severa)";
-    } else {
-        return "Obesidade Grau III (mórbida)";
+async function cadastrarNaAPI(objIMC) {
+
+  try {
+      let resposta = await fetch("http://localhost:3000/imc", {
+      method: "POST",
+      body: JSON.stringify(objIMC),
+      headers : {
+          "Content-Type" : "application/json; charset=UTF-8"
+      }
+       });
+       return true;
+
+  } catch (error) {
+
+    console.log(error);
+    return false;
+
+  }
+}
+
+
+async function buscarIMCs(){
+    try {
+       const retorno = await fetch("http://localhost:3000/imc");
+       const dadosRetornados = await retorno.json();
+
+       console.log(dadosRetornados);
+
+       const tabela = document.getElementById("cadastro");
+
+
+       for (let i = 0; i < dadosRetornados.length; i++) {
+
+        tabela.innerHTML += 
+        `<tr>
+        <td>${dadosRetornados[i].nome}</td>
+        <td>${dadosRetornados[i].altura}</td>
+        <td>${dadosRetornados[i].peso}</td>
+        <td>${dadosRetornados[i].IMC.toFixed(2)}</td>
+        <td>${dadosRetornados[i].situacao}</td>
+        </tr>`;
+       }
+
+       
+
+    } catch (error) {
+        console.log(error);
     }
 }
-
